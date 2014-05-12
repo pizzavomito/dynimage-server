653626a0177bf0d3a8dc9503ff1b0e5acb0f7a9e
Pascal <Pascal@Pascal-PC>
2014-05-12 10:07:58 +0200

    simplification

--- src/app.php
+++ src/app.php
@@ -1,26 +1,34 @@
 <?php
-/** /
+/**/
 use DynImage\Filter\Resize;
 use DynImage\Filter\Border;
 use DynImage\Filter\Colorize;
 use DynImage\Filter\Reflect;
+use DynImage\Filter\Experimental\WatermarkText;
 use DynImage\DynImage;
-use DynImage\Transformer;
+use DynImage\Events;
 
-$transformer = new Transformer();
-$transformer->add(new Resize(array('height' => 200, 'width' => 200)));
-$transformer->add(new Colorize(array('color' => '#ff9900')));
-$transformer->add(new Border(array('height' => 6, 'width' => 6, 'color', '#000')));
-$transformer->add(new Reflect());
+
+$di = new DynImage();
+
+$di->add(new Resize(array('height' => 200, 'width' => 200)));
+//$di->add(new Colorize(array('color' => '#ff9900')));
+//$di->add(new WatermarkText(array('text' => 'DSK', 'position' => 'center', 'font_size' => 150, 'font_color' => '#ffffff')),Events::FINISH_CREATE_IMAGE);
+$di->add(new Border(array('height' => 6, 'width' => 6, 'color', '#000')));
+
+//$di->add(new Reflect());
 
 $filename = 'http://cdn-parismatch.ladmedia.fr/var/news/storage/images/paris-match/actu/politique/dominique-strauss-kahn-a-accepte-de-se-soumettre-a-des-test-adn-149718/1455872-1-fre-FR/DSK-soumis-a-des-test-ADN_article_landscape_pm_v8.jpg';
 
-$image = DynImage::getImage($transformer, file_get_contents($filename), $filename);
+$image = $di->apply(file_get_contents($filename));
+//$image = DynImage::getImage($transformer, file_get_contents($filename), $filename);
 
 $image->show('png');
-/**/
+
 
 /**/
+
+/** /
 use Silex\Provider\HttpCacheServiceProvider;
 use Silex\Provider\MonologServiceProvider;
 
